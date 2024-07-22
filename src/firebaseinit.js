cea2a0e646ee449be7c952206008f789e642c11e
ramswarooppatidar <ramkatariya1603@gmail.com>
2024-07-22 13:28:56 +0530

    firebase update

--- src/firebaseinit.js
+++ src/firebaseinit.js
@@ -6,14 +6,36 @@ import { getFirestore } from 'firebase/firestore/lite';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB04PuTslc3ry8oB8bXdLF7dhdY7TMwtrM",
-  authDomain: "busybuy-app-d82a8.firebaseapp.com",
-  projectId: "busybuy-app-d82a8",
-  storageBucket: "busybuy-app-d82a8.appspot.com",
-  messagingSenderId: "701928866214",
-  appId: "1:701928866214:web:0618d5e31d110436d44f67"
+  apiKey: "AIzaSyCJOj39AlXo5KbUhern_Sh6EiqtEZkFaSs",
+  authDomain: "busybuy-fire.firebaseapp.com",
+  projectId: "busybuy-fire",
+  storageBucket: "busybuy-fire.appspot.com",
+  messagingSenderId: "724216360690",
+  appId: "1:724216360690:web:bf62d557894c40d305aa6b"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+
+
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from 'firebase/firestore/lite';
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB04PuTslc3ry8oB8bXdLF7dhdY7TMwtrM",
+//   authDomain: "busybuy-app-d82a8.firebaseapp.com",
+//   projectId: "busybuy-app-d82a8",
+//   storageBucket: "busybuy-app-d82a8.appspot.com",
+//   messagingSenderId: "701928866214",
+//   appId: "1:701928866214:web:0618d5e31d110436d44f67"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// export const db = getFirestore(app);
